0c5f0ba125c674ae8d3c5a3f1a1afb8030a003cb
zzo3ozz <90233522+zzo3ozz@users.noreply.github.com>
2023-02-22 15:27:16 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 248 ms, Memory: 21984 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 
 public class Main {
 	public static int N;
-	public static long temp = 0; // 계산값을 저장할 변수
 	public static int[] array; // 입력된 수열을 저장할 배열
 	public static int[] operator = new int[4]; // 연산자의 정보를 저장할 배열
 	public static ArrayList result = new ArrayList<Long>(); // 계산된 값을 저장할 ArrayList
@@ -26,51 +25,41 @@ public class Main {
 			operator[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		calculate(0);
+		calculate(0, array[0]);
 		
 		Collections.sort(result); // 계산된 값을 오름차순으로 정렬
 		
-		System.out.print(result.get(result.size() - 1) + "\n" + result.get(0));
+		System.out.print(result.get(result.size() - 1)+ "\n" + result.get(0));
 		
 		br.close();
 	}
 	
 	// 모든 연산자를 계산할 dfs 탐색 함수
-	public static void calculate(int depth) {
+	public static void calculate(int depth, int num) {
 		// 모든 연산자를 사용하였으면(수 배열의 모든 값을 사용하였으면) 계산값을 저장하고 탐색 종료
-		if(depth == N) {
-			result.add(temp);
+		if(depth == N - 1) {
+			result.add(num);
 			return;
-		}
-		
-		// depth가 0일 떄, 계산값을 저장할 temp를 첫번째 수로 지정
-		if(depth == 0) {
-			temp = array[0];
-			calculate(depth + 1); //다음 깊이(= 다음 연산자) 탐색
 		}
 		
 		for(int i = 0; i < 4; i++) {
 			if(operator[i] != 0) {
 				operator[i]--; // operator를 사용했으므로 해당 연산자 값 하나 줄이기
-				if(i == 0) { // 덧셈 연산자
-					temp += array[depth];
-					calculate(depth + 1);
-					temp -= array[depth];
-				} else if(i == 1) { // 뺄셈 연산자
-					temp -= array[depth];
-					calculate(depth + 1);
-					temp += array[depth];
-				} else if(i == 2) { // 곱셈 연산자
-					temp *= array[depth];
-					calculate(depth + 1);
-					temp /= array[depth];
-				} else { // 나눗셈 연산자
-					// 나머지를 기억해두어야 depth + 1 탐색 후 돌아왔을 때 원래 값으로 되돌릴 수 있음
-					long remainder = temp % array[depth];
-					temp /= array[depth];
-					calculate(depth + 1);
-					temp *= array[depth];
-					temp += remainder;
+				// 다른 풀이 참고 후 변경 점 : if - else 구문을 switch-case구문으로,
+				// 전역변수를 통해 (계산 - 탐색 후 역계산)하던 과정을 매개변수 전달로 간편히 함
+				switch(i) {
+				case 0:
+					calculate(depth + 1, num + array[depth + 1]);
+					break;
+				case 1:
+					calculate(depth + 1, num - array[depth + 1]);
+					break;
+				case 2:
+					calculate(depth + 1, num * array[depth + 1]);
+					break;
+				case 3:
+					calculate(depth + 1, num / array[depth + 1]);
+					break;
 				}
 				operator[i]++; // operator 사용이 끝났으므로 해당 연산자 값 다시 늘리기
 			}
